437380c171e7a59747414f7ca2ece3595fe21018
Kostya <vertogpro@gmail.com>
2025-07-25 16:58:37 +0500

    start room example (not work yet)

---
+++ Assets/Examples/Mirror.Example/Scripts/RoomClientBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Examples.Mirror.Example
+{
+    public class RoomClientBehaviour : NetworkRoomPlayer
+    {
+        [SerializeField] private GameObject playerPrefab;
+        
+        public override void OnStartAuthority()
+        {
+            Debug.Log(isClient);
+            CmdSpawnPlayer();
+        }
+
+        [Command]
+        private void CmdSpawnPlayer()
+        {
+            Debug.Log(nameof(CmdSpawnPlayer));
+            var playerObj = Instantiate(playerPrefab, transform);
+            NetworkServer.Spawn(playerObj, netIdentity.connectionToClient);
+        }
+    }
+}
